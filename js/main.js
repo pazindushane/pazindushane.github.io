cc358f9cf8ef98c4d06904e1c12d3ff1c207f9c3
pazindushane <shanepazindu7399@gmail.com>
2022-07-28 12:11:10 +0530

    website updated

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@
     
     
     // Typed Initiate
-    if ($('.hero .hero-text h2').length == 1) {
+    if ($('.hero .hero-text h2').length === 1) {
         var typed_strings = $('.hero .hero-text .typed-text').text();
         var typed = new Typed('.hero .hero-text h2', {
             strings: typed_strings.split(', '),
--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
+                url: "mail/contact.php",
                 type: "POST",
                 data: {
                     name: name,
